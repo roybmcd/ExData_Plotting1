0dbf4e60a2cf6324cbd3f70996b949b19e2b2184
RoyBMcD <roybmcd@gmail.com>
2015-06-07 19:01:05 -0600

    Fixed some issues with the legend.
    Signed-off-by: RoyBMcD <roybmcd@gmail.com>

--- plot4.R
+++ plot4.R
@@ -23,17 +23,22 @@ table$date_time <-as.POSIXlt(paste(table$Date, table$Time), format="%d/%m/%Y H%:
 table$date_time <-paste(table$Date, table$Time)
 table$date_time <-strptime(table$date_time, "%d/%m/%Y %H:%M:%S")
 
+## create a function to create plot3
+plot3<-function (){
+	##Plot a graph of global active power vs time
+	with(table,plot(date_time,Sub_metering_1, xlab="",ylab = "Energy sub metering", type="n"))
+	with(table,lines(date_time,Sub_metering_1))
+	with(table,lines(date_time,Sub_metering_2,col="red"))
+	with(table,lines(date_time,Sub_metering_3, col="blue"))
+	legend("topright", lty = 1, bty="n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+}
+
 ##Plot a graph of global active power vs time
 par(mfrow=c(2,2))
 with(table,{
 	plot(date_time,Global_active_power, type = "l", xlab="",ylab = "Global Active Power")
 	plot(date_time,Voltage, type = "l", xlab="datetime",ylab = "Voltage")
-	with(table,{
-		plot(date_time,Sub_metering_1, xlab="",ylab+"Energy sub metering",type="n")
-		lines(date_time,Sub_metering_1)
-		lines(date_time,Sub_metering_2, col="red")
-		lines(date_time,Sub_metering_3, col="blue")
-	})			
+	plot3()			
 	plot(date_time,Global_reactive_power, type = "l", xlab="datetime",)
 })
 
