94d0fcd886e856da7119f0f9c2caaf0c69bd2812
RoyBMcD <roybmcd@gmail.com>
2015-06-07 19:04:58 -0600

    Signed-off-by: RoyBMcD <roybmcd@gmail.com>

--- plot3.R
+++ plot3.R
@@ -28,7 +28,7 @@ with(table,plot(date_time,Sub_metering_1, xlab="",ylab = "Energy sub metering", 
 with(table,lines(date_time,Sub_metering_1))
 with(table,lines(date_time,Sub_metering_2,col="red"))
 with(table,lines(date_time,Sub_metering_3, col="blue"))
-
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ##Save the plot as Plot3.png
 
